a7aef659f6d87d8ed1b7d8b0842f6f49ed2e40b0
Fernando Washington <ironitabarbosa@gmail.com>
2025-03-02 15:56:00 -0400

    primary logic finished

--- currency-converter/script/script.js
+++ currency-converter/script/script.js
@@ -1,8 +1,52 @@
 let optionFrom = document.getElementById("optionFrom");
 let optionTo = document.getElementById("optionTo");
+let resultAmount = document.getElementById("amountInput");
+let result = document.getElementById("result");
 
 function toggleSelect() {
   let temp = optionFrom.value;
   optionFrom.value = optionTo.value;
   optionTo.value = temp;
+}
+
+function teste() {
+  if (optionFrom.value === "brl" && optionTo.value === "usd") {
+    finalResult = resultAmount.value / 5.88;
+    result.innerHTML = finalResult.toFixed(2);
+    console.log(finalResult);
+  } else if (optionFrom.value === "usd" && optionTo.value === "brl") {
+    finalResult = resultAmount.value * 5.88;
+    result.innerHTML = finalResult.toFixed(2);
+    console.log(finalResult);
+  } else if (optionFrom.value === "brl" && optionTo.value === "eur") {
+    finalResult = resultAmount.value / 6.105;
+    result.innerHTML = finalResult.toFixed(2);
+    console.log(finalResult); 
+  } else if (optionFrom.value === "eur" && optionTo.value === "brl") {
+    finalResult = resultAmount.value * 6.105;
+    result.innerHTML = finalResult.toFixed(2);
+    console.log(finalResult);
+  } else if (optionFrom.value === "brl" && optionTo.value === "brl") {
+    finalResult = resultAmount.value
+    result.innerHTML = finalResult
+    console.log(finalResult)
+  } else if (optionFrom.value === "eur" && optionTo.value === "usd") {
+    finalResult = resultAmount.value / 0.9639;
+    result.innerHTML = finalResult.toFixed(2)
+    console.log(finalResult)
+  } else if (optionFrom.value === "usd" && optionTo.value === "eur") {
+    finalResult = resultAmount.value / 1.038;
+    result.innerHTML = finalResult.toFixed(2)
+    console.log(finalResult)
+  } else if (optionFrom.value === "usd" && optionTo.value === "usd") {
+    finalResult = resultAmount.value
+    result.innerHTML = finalResult
+    console.log(finalResult) 
+  } else if (optionFrom.value === "eur" && optionTo.value === "eur") {
+    finalResult = resultAmount.value
+    result.innerHTML = finalResult
+    console.log(finalResult)
+  } else {
+    console.log('Error')   
+  }
 }
