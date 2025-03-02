0a2c52b0647987bed7a898a6546b79b6e9c9a668
Fernando Washington <ironitabarbosa@gmail.com>
2025-03-02 16:20:27 -0400

    add showResult function and validation of input

--- currency-converter/script/script.js
+++ currency-converter/script/script.js
@@ -2,6 +2,7 @@ let optionFrom = document.getElementById("optionFrom");
 let optionTo = document.getElementById("optionTo");
 let resultAmount = document.getElementById("amountInput");
 let result = document.getElementById("result");
+let resultHtml = document.querySelector(".result-container");
 
 function toggleSelect() {
   let temp = optionFrom.value;
@@ -9,7 +10,18 @@ function toggleSelect() {
   optionTo.value = temp;
 }
 
+function showResult() {
+  if (resultHtml.style.display === "none") {
+    resultHtml.style.display = "flex";
+  } else {
+    resultHtml.style.display = "none";
+  } 
+}
+
 function teste() {
+  // primary logic convert
+  // add R$ and more, in the final of the result
+
   if (optionFrom.value === "brl" && optionTo.value === "usd") {
     finalResult = resultAmount.value / 5.88;
     result.innerHTML = finalResult.toFixed(2);
@@ -21,32 +33,42 @@ function teste() {
   } else if (optionFrom.value === "brl" && optionTo.value === "eur") {
     finalResult = resultAmount.value / 6.105;
     result.innerHTML = finalResult.toFixed(2);
-    console.log(finalResult); 
+    console.log(finalResult);
   } else if (optionFrom.value === "eur" && optionTo.value === "brl") {
     finalResult = resultAmount.value * 6.105;
     result.innerHTML = finalResult.toFixed(2);
     console.log(finalResult);
   } else if (optionFrom.value === "brl" && optionTo.value === "brl") {
-    finalResult = resultAmount.value
-    result.innerHTML = finalResult
-    console.log(finalResult)
+    finalResult = resultAmount.value;
+    result.innerHTML = finalResult;
+    console.log(finalResult);
   } else if (optionFrom.value === "eur" && optionTo.value === "usd") {
     finalResult = resultAmount.value / 0.9639;
-    result.innerHTML = finalResult.toFixed(2)
-    console.log(finalResult)
+    result.innerHTML = finalResult.toFixed(2);
+    console.log(finalResult);
   } else if (optionFrom.value === "usd" && optionTo.value === "eur") {
     finalResult = resultAmount.value / 1.038;
-    result.innerHTML = finalResult.toFixed(2)
-    console.log(finalResult)
+    result.innerHTML = finalResult.toFixed(2);
+    console.log(finalResult);
   } else if (optionFrom.value === "usd" && optionTo.value === "usd") {
-    finalResult = resultAmount.value
-    result.innerHTML = finalResult
-    console.log(finalResult) 
+    finalResult = resultAmount.value;
+    result.innerHTML = finalResult;
+    console.log(finalResult);
   } else if (optionFrom.value === "eur" && optionTo.value === "eur") {
-    finalResult = resultAmount.value
-    result.innerHTML = finalResult
-    console.log(finalResult)
+    finalResult = resultAmount.value;
+    result.innerHTML = finalResult;
+    console.log(finalResult);
   } else {
-    console.log('Error')   
+    console.log("Error");
   }
+
+  // validation 
+
+  if (resultAmount.value) {
+   showResult()
+  } else {
+    showResult()
+    result.innerHTML = 'Invalid'
+  }
+
 }
