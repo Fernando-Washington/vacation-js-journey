3837d3ce1e299c5c12f28ef869e1821335c2ad83
Fernando Washington <ironitabarbosa@gmail.com>
2025-03-03 13:34:23 -0400

    add dark mode

--- currency-converter/script/script.js
+++ currency-converter/script/script.js
@@ -3,6 +3,31 @@ let optionTo = document.getElementById("optionTo");
 let resultAmount = document.getElementById("amountInput");
 let result = document.getElementById("result");
 let resultHtml = document.querySelector(".result-container");
+let moonIcon = document.querySelector(".fa-moon");
+let sunIcon = document.querySelector(".fa-sun");
+let root = document.documentElement //?
+
+function toggleDarkMode(){
+  if (moonIcon.style.display === 'block' && sunIcon.style.display === 'none') {
+    root.style.setProperty("--font-color", " #B4B4B4");
+    root.style.setProperty("--bg-color", "rgb(24, 22, 22)");
+    root.style.setProperty("--bg-secondary-color", " #2e2e2e");
+    root.style.setProperty("--secundary-color", " #659CA3");
+    root.style.setProperty("--input-color", " #343434");
+
+    moonIcon.style.display = 'none';
+    sunIcon.style.display = 'block';
+  } else {
+    root.style.setProperty("--font-color", " #374151");
+    root.style.setProperty("--bg-color", " #F3F4F6");
+    root.style.setProperty("--bg-secondary-color", " #FFFFFF");
+    root.style.setProperty("--secundary-color", " #4F46E5");
+    root.style.setProperty("--input-color", " #EFEFEF");
+
+    moonIcon.style.display = 'block';
+    sunIcon.style.display = 'none';
+  }
+}
 
 function toggleSelect() {
   let temp = optionFrom.value;
@@ -15,7 +40,7 @@ function showResult() {
     resultHtml.style.display = "flex";
   } else {
     resultHtml.style.display = "none";
-  } 
+  }
 }
 
 function teste() {
@@ -62,13 +87,12 @@ function teste() {
     console.log("Error");
   }
 
-  // validation 
+  // validation
 
   if (resultAmount.value) {
-   showResult()
+    showResult();
   } else {
-    showResult()
-    result.innerHTML = 'Invalid'
+    showResult();
+    result.innerHTML = "Invalid";
   }
-
 }
