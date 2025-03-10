d423c607300831786ebeec10379070502d77337e
Fernando Washington <ironitabarbosa@gmail.com>
2025-03-10 19:21:19 -0400

    add currency converter API

--- currency-converter/script/script.js
+++ currency-converter/script/script.js
@@ -5,18 +5,18 @@ let result = document.getElementById("result");
 let resultHtml = document.querySelector(".result-container");
 let moonIcon = document.querySelector(".fa-moon");
 let sunIcon = document.querySelector(".fa-sun");
-let root = document.documentElement //?
+let root = document.documentElement; //?
 
-function toggleDarkMode(){
-  if (moonIcon.style.display === 'block' && sunIcon.style.display === 'none') {
+function toggleDarkMode() {
+  if (moonIcon.style.display === "block" && sunIcon.style.display === "none") {
     root.style.setProperty("--font-color", " #B4B4B4");
     root.style.setProperty("--bg-color", "rgb(24, 22, 22)");
     root.style.setProperty("--bg-secondary-color", " #2e2e2e");
     root.style.setProperty("--secundary-color", " #659CA3");
     root.style.setProperty("--input-color", " #343434");
 
-    moonIcon.style.display = 'none';
-    sunIcon.style.display = 'block';
+    moonIcon.style.display = "none";
+    sunIcon.style.display = "block";
   } else {
     root.style.setProperty("--font-color", " #374151");
     root.style.setProperty("--bg-color", " #F3F4F6");
@@ -24,8 +24,8 @@ function toggleDarkMode(){
     root.style.setProperty("--secundary-color", " #4F46E5");
     root.style.setProperty("--input-color", " #EFEFEF");
 
-    moonIcon.style.display = 'block';
-    sunIcon.style.display = 'none';
+    moonIcon.style.display = "block";
+    sunIcon.style.display = "none";
   }
 }
 
@@ -43,48 +43,74 @@ function showResult() {
   }
 }
 
-function teste() {
+const URL = "https://api.exchangerate-api.com/v4/latest/USD";
+
+let BRL, USD, EUR; // Variáveis globais
+
+async function chamarApi() {
+  const resp = await fetch(URL);
+  if (resp.status === 200) {
+    const data = await resp.json();
+    BRL = data.rates.BRL;
+    USD = data.rates.USD;
+    EUR = data.rates.EUR;
+  }
+}
+
+async function teste() {
   // primary logic convert
   // add R$ and more, in the final of the result
+  await chamarApi();
 
-  if (optionFrom.value === "brl" && optionTo.value === "usd") {
-    finalResult = resultAmount.value / 5.88;
-    result.innerHTML = finalResult.toFixed(2);
-    console.log(finalResult);
-  } else if (optionFrom.value === "usd" && optionTo.value === "brl") {
-    finalResult = resultAmount.value * 5.88;
-    result.innerHTML = finalResult.toFixed(2);
-    console.log(finalResult);
-  } else if (optionFrom.value === "brl" && optionTo.value === "eur") {
-    finalResult = resultAmount.value / 6.105;
-    result.innerHTML = finalResult.toFixed(2);
-    console.log(finalResult);
-  } else if (optionFrom.value === "eur" && optionTo.value === "brl") {
-    finalResult = resultAmount.value * 6.105;
-    result.innerHTML = finalResult.toFixed(2);
-    console.log(finalResult);
-  } else if (optionFrom.value === "brl" && optionTo.value === "brl") {
-    finalResult = resultAmount.value;
-    result.innerHTML = finalResult;
-    console.log(finalResult);
-  } else if (optionFrom.value === "eur" && optionTo.value === "usd") {
-    finalResult = resultAmount.value / 0.9639;
-    result.innerHTML = finalResult.toFixed(2);
-    console.log(finalResult);
-  } else if (optionFrom.value === "usd" && optionTo.value === "eur") {
-    finalResult = resultAmount.value / 1.038;
-    result.innerHTML = finalResult.toFixed(2);
-    console.log(finalResult);
-  } else if (optionFrom.value === "usd" && optionTo.value === "usd") {
-    finalResult = resultAmount.value;
-    result.innerHTML = finalResult;
-    console.log(finalResult);
-  } else if (optionFrom.value === "eur" && optionTo.value === "eur") {
-    finalResult = resultAmount.value;
-    result.innerHTML = finalResult;
-    console.log(finalResult);
+  if (BRL && USD && EUR) {
+    let finalResult;
+
+    if (optionFrom.value === "brl" && optionTo.value === "usd") {
+      finalResult = resultAmount.value / BRL;
+      result.innerHTML = finalResult.toFixed(2);
+      console.log(finalResult);
+    } else if (optionFrom.value === "usd" && optionTo.value === "brl") {
+      finalResult = resultAmount.value * BRL;
+      result.innerHTML = finalResult.toFixed(2);
+      console.log(finalResult);
+
+    } else if (optionFrom.value === "brl" && optionTo.value === "eur") {
+      finalResult = (resultAmount.value / BRL) * EUR;
+      result.innerHTML = finalResult.toFixed(2);
+      console.log(finalResult);
+    } else if (optionFrom.value === "eur" && optionTo.value === "brl") {
+      finalResult = (resultAmount.value * BRL) / EUR;
+      result.innerHTML = finalResult.toFixed(2);
+
+      console.log(finalResult);
+    } else if (optionFrom.value === "brl" && optionTo.value === "brl") {
+      finalResult = resultAmount.value;
+      result.innerHTML = finalResult;
+      console.log(finalResult);
+
+    } else if (optionFrom.value === "eur" && optionTo.value === "usd") {
+      finalResult = resultAmount.value / USD;
+      result.innerHTML = finalResult.toFixed(2);
+      console.log(finalResult);
+    } else if (optionFrom.value === "usd" && optionTo.value === "eur") {
+      finalResult = resultAmount.value / EUR;
+      result.innerHTML = finalResult.toFixed(2);
+      console.log(finalResult);
+
+    } else if (optionFrom.value === "usd" && optionTo.value === "usd") {
+      finalResult = resultAmount.value;
+      result.innerHTML = finalResult;
+      console.log(finalResult);
+
+    } else if (optionFrom.value === "eur" && optionTo.value === "eur") {
+      finalResult = resultAmount.value;
+      result.innerHTML = finalResult;
+      console.log(finalResult);
+    } else {
+      console.log("Error");
+    }
   } else {
-    console.log("Error");
+    console.log("Erro: as taxas de câmbio não foram carregadas");
   }
 
   // validation
@@ -96,3 +122,4 @@ function teste() {
     result.innerHTML = "Invalid";
   }
 }
+teste();
