16db2b96c3ca8671d4276708ee0e519c3439f634
Fernando Washington <ironitabarbosa@gmail.com>
2025-03-17 23:32:15 -0400

    feat: add logic showDetails

--- weather-app/script/weather.js
+++ weather-app/script/weather.js
@@ -1,6 +1,14 @@
 const apiKey = "42e9ab3e743bbfe2ff51939065e77744";
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=${apiKey}`;
 
+const local = document.getElementById('input-local');
+const degree = document.getElementById('degrees')
+
+// add validacao de str
+const showDetails = () => {
+  degree.innerHTML=local.value
+}
+
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
