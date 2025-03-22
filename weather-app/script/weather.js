48057c03a6a92914397dfd16997b1758f6544729
Fernando Washington <152669048+Fernando-Washington@users.noreply.github.com>
2025-03-21 23:54:10 -0400

    fix: features when consuming weather API

--- weather-app/script/weather.js
+++ weather-app/script/weather.js
@@ -1,19 +1,29 @@
-const apiKey = "42e9ab3e743bbfe2ff51939065e77744";
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=${apiKey}`;
+// carregando variavel de ambiente 
+require('dotenv').config()
 
-const local = document.getElementById('input-local');
-const degree = document.getElementById('degrees')
+const apiKey = process.env.API_KEY
+const city = 'São Paulo,BR'
 
-// add validacao de str
-const showDetails = () => {
-  degree.innerHTML=local.value
-}
+const temp_result = document.getElementById('temp_result')
+const humidity_result = document.getElementById('humidity_result')
+const wind_result = document.getElementById('wind_result')
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city} &units=metric&appid=${apiKey}`
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const temp = data.main.temp
+      const humidity = data.main.humidity
+      const wind = data.wind.speed
+
+      temp_result.innerHTML = temp
+      humidity_result.innerHTML = humidity
+      wind_result.innerHTML = humidity
+      console.log(temp, humidity, wind)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
