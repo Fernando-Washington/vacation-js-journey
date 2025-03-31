7216b529ce125c33c0ee41f6c30606e810fff05a
Fernando Washington <ironitabarbosa@gmail.com>
2025-03-30 21:10:38 -0400

    feat: "AddTask" function finished

--- to-do-list/script/script.js
+++ to-do-list/script/script.js
@@ -1,3 +1,18 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+const AddTask = () => {
+    if (inputBox.value === '') {
+        alert("you need to add something");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        inputBox.value = ''
+    }
+};
+
 const Delete = () => {
-    alert('ok')
-}
+    alert("ok");
+};
