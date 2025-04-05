0d5a3fcdd22efd2c7d8863867a0312b624bbc50a
Fernando Washington <ironitabarbosa@gmail.com>
2025-04-04 23:13:36 -0400

    feat: add close modal function

---
+++ cadastro-clientes/script/script.js
@@ -0,0 +1,6 @@
+const closeModal = document.querySelector('.modal-register i')
+const modal = document.querySelector('.modal-register')
+
+closeModal.onclick = () => {
+    modal.style.display = 'none'
+}
