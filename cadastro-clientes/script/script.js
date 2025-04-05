e7af67de4ff4c7090fd3da907c33cd19c8a3070b
Fernando Washington <ironitabarbosa@gmail.com>
2025-04-04 23:23:46 -0400

    feat: add open modal function

--- cadastro-clientes/script/script.js
+++ cadastro-clientes/script/script.js
@@ -1,5 +1,10 @@
 const closeModal = document.querySelector('.modal-register i')
+const openModal = document.querySelector('.open-register')
 const modal = document.querySelector('.modal-register')
+
+openModal.onclick = () => {
+    modal.style.display = 'block'
+}
 
 closeModal.onclick = () => {
     modal.style.display = 'none'
